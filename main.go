f028f7dbd7031abc95fad59d5dde7877d4a6f85d
akrolayer <almagestf532@gmail.com>
2021-01-25 23:37:46 +0900

    problem2

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
+
+	fmt.Println("problem1 =", problem1())
+	fmt.Println("problem2 =", problem2())
+}
+
+func problem1() int {
 	triple := 0
 	penta := 0
 	fifty := 0
@@ -15,5 +21,22 @@ func main() {
 			fifty += i
 		}
 	}
-	fmt.Println(triple + penta + fifty)
+	return triple + penta + fifty
+}
+
+func problem2() int {
+	var memo [2000]int
+	memo[0] = 1
+	memo[1] = 1
+	sum := 0
+	for i := 2; i < 2000; i++ {
+		memo[i] = memo[i-2] + memo[i-1]
+		if memo[i] > 4000000 {
+			break
+		}
+		if memo[i]%2 == 0 {
+			sum += memo[i]
+		}
+	}
+	return sum
 }
